78921dc6fadc1c2d6f8da8aee36b74f77342938f
shahisushil <shahisushil98@gamil.com>
2024-03-11 11:28:25 +0545

    'update

---
+++ main.py
@@ -0,0 +1,42 @@
+# name = 'Sushil'
+# age = 40
+# email = 'shahisushil98@gmail.com'
+# phone = 9767547742
+# address = 'Gyaneshwor'
+# print(f"My name is {name} My age is {age} My email is {email} My phone number is {phone} My address  {address}")
+# print ("hello python")
+
+# data=[
+#     [12,34,56,78,97]
+#     [67,87,[[[600]],900],12,34,54]
+# ]
+# print (data[1][4])
+
+# data=[
+#     [34,56,78],
+#     [55,77,88]
+# ]
+
+# data[0].pop(0)
+# data[1].pop(2)
+# print(data)
+
+
+data=[
+    [34,56,78],
+    [55,77,88]
+]
+
+
+# data[0].insert(1,200)
+# data[1].insert(2,500)
+# print(data)
+
+
+# data[0].append(500)
+# data[1].append(500)
+# print(data)
+
+data[0].pop(1)
+data[1].pop(2)
+print(data)
